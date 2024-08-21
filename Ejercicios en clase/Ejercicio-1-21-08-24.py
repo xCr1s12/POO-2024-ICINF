cbc52eefaa4e6bec0035a1de1ffabf1b1f347601
xCr1s12 <CristobalAlexis.onate@alumnos.ulagos.cl>
2024-08-21 11:55:25 -0400

    ....02

--- Ejercicios en clase/Ejercicio-1-21-08-24.py
+++ Ejercicios en clase/Ejercicio-1-21-08-24.py
@@ -1,7 +1,17 @@
 class Persona():
-    def __init__(self, altura):
-        
+    def __init__(self, nombre, edad, altura, peso):
+        self.nombre = nombre
+        self.edad = edad
+    def imc(self, altura, peso):
+        self.altura = altura
+        self.peso = peso
+        print(f"{self.peso / (self.altura**2)}")
 
+persona1 = Persona("Cristobal", 19)
+print(f"Hola {persona1.nombre}, tienes {persona1.edad}")
+
+
+persona1.imc(1.80, 75)
 
 
 
